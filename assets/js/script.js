89f1d813827c7abe52281db5a175a805e0ebbfe4
Paul Worthington <pworthington17@gmail.com>
2024-06-02 07:36:26 +0000

    game area added

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame () {
+
+}
+
+function selectWords () {
+
+}
+
+function displayWords () {
+
+}
+
+function identifyCorrect () {
+
+}
+
+function identifyIncorrect () {
+
+}
+
+function incrimentCorrect () {
+
+}
+
+function incrimentIncorrect () {
+
+}
+
+function gameComplete () {
+    
+}
