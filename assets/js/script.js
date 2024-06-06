8e95d31507219781222ab4ff06f5a1c6dd67ad4e
Paul Worthington <pworthington17@gmail.com>
2024-06-06 07:03:46 +0000

    change game running to function

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,126 +13,97 @@ let houseWords = ["Kitchen Cuisine", "Carpet Tapis", "Fridge Frigo", "Freezer Co
 let restaruantWords = ["Bill Addition", "Starter Entrée", "Dish Plat", "Desert Dessert", "Coffee Café", "Tea Thé"]
 
 
-/** The Fisher-Yates shuffle
- * https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
- * https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
- */
-function shuffle(array) {
-        let currentIndex = array.length;
-      
-        while (currentIndex != 0) {
-      
-          let randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex--;
-      
-          [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]];
-        }
-}
 
-const schoolWordsBase = ["Pen Stylo", "Pencil Crayon", "Calculator Calculatrice", "Book Livre", "Dictionary Dictionnaire", "Student Étudiante", "Teacher Professeur", "Math Mathématiques", "English Anglais", "History Histoire", "Science Science", "Homework Devoirs", "Pencilcase Trouse", "Eraser Gomme", "Protractor Rapporteur"]
 
+let schoolWordsBase = ["Pen Stylo", "Pencil Crayon", "Calculator Calculatrice", "Book Livre", "Dictionary Dictionnaire", "Student Étudiante", "Teacher Professeur", "Math Mathématiques", "English Anglais", "History Histoire", "Science Science", "Homework Devoirs", "Pencilcase Trouse", "Eraser Gomme", "Protractor Rapporteur"]
+let schoolWords = ["Pen Stylo", "Pencil Crayon", "Calculator Calculatrice", "Book Livre", "Dictionary Dictionnaire", "Student Étudiante", "Teacher Professeur", "Math Mathématiques", "English Anglais", "History Histoire", "Science Science", "Homework Devoirs", "Pencilcase Trouse", "Eraser Gomme", "Protractor Rapporteur"]
+let schoolContainersEnglish = ["school-english-word-1", "school-english-word-2", "school-english-word-3", "school-english-word-4", "school-english-word-5", "school-english-word-6"];
+let schoolContainersFrench = ["school-french-word-1", "school-french-word-2", "school-french-word-3", "school-french-word-4", "school-french-word-5", "school-french-word-6"];
+
+gameSchool(schoolWords, schoolContainersEnglish, schoolContainersFrench)
 
 //working on creating this function 
-function game (a, b, c) {
+function gameSchool (a, b, c) {
         let schoolWords = a;
+        shuffle(schoolWords);
         let containersEnglish = b;
         let containersFrench = c;
+        shuffle(containersEnglish);
+        shuffle(containersFrench);
+
+        //to remove reandomly slected array item 
+        let firstArray = schoolWords.shift();
+        let secondArray = schoolWords.shift();
+        let thirdArray = schoolWords.shift();
+        let forthArray = schoolWords.shift();
+        let fifthArray = schoolWords.shift();
+        let sixthArray = schoolWords.shift();
+
+        var values = firstArray.split(" ");
+        var englishWordOne = values[0];
+        var frenchWordOne = firstArray.substring(firstArray.indexOf(' ') + 1);
+        
+        //to select random container 
+        let englishContainerOne = containersEnglish.shift();
+        let frenchContainerOne = containersFrench.shift();
+
+        //to send random word to random container
+        document.getElementById(englishContainerOne).textContent = englishWordOne;
+        document.getElementById(frenchContainerOne).textContent = frenchWordOne;
+        
+        //repeat of the above code 
+        var values = secondArray.split(" ");
+        var englishWordTwo = values[0];
+        var frenchWordTwo = secondArray.substring(secondArray.indexOf(' ') + 1);
+        
+        let englishContainerTwo = containersEnglish.shift();
+        let frenchContainerTwo = containersFrench.shift();
+
+        document.getElementById(englishContainerTwo).textContent = englishWordTwo;
+        document.getElementById(frenchContainerTwo).textContent = frenchWordTwo;
+        
+        var values = thirdArray.split(" ");
+        var englishWordThree = values[0];
+        var frenchWordThree = thirdArray.substring(thirdArray.indexOf(' ') + 1);
+        
+        let englishContainerThree = containersEnglish.shift();
+        let frenchContainerThree = containersFrench.shift();
+
+        document.getElementById(englishContainerThree).textContent = englishWordThree;
+        document.getElementById(frenchContainerThree).textContent = frenchWordThree;
+        
+        var values = forthArray.split(" ");
+        var englishWordFour = values[0];
+        var frenchWordFour = forthArray.substring(forthArray.indexOf(' ') + 1);
+        
+        let englishContainerFour = containersEnglish.shift();
+        let frenchContainerFour = containersFrench.shift();
+
+        document.getElementById(englishContainerFour).textContent = englishWordFour;
+        document.getElementById(frenchContainerFour).textContent = frenchWordFour;
+        
+        var values = fifthArray.split(" ");
+        var englishWordFive = values[0];
+        var frenchWordFive = fifthArray.substring(fifthArray.indexOf(' ') + 1);
+        
+        let englishContainerFive = containersEnglish.shift();
+        let frenchContainerFive = containersFrench.shift();
+
+        document.getElementById(englishContainerFive).textContent = englishWordFive;
+        document.getElementById(frenchContainerFive).textContent = frenchWordFive;
+        
+        var values = sixthArray.split(" ");
+        var englishWordSix = values[0];
+        var frenchWordSix = sixthArray.substring(sixthArray.indexOf(' ') + 1);
+        
+        let englishContainerSix = containersEnglish.shift();
+        let frenchContainerSix = containersFrench.shift();
+
+        document.getElementById(englishContainerSix).textContent = englishWordSix;
+        document.getElementById(frenchContainerSix).textContent = frenchWordSix;
+        // end of repeat 
+
 }
-
-
-let schoolWords = ["Pen Stylo", "Pencil Crayon", "Calculator Calculatrice", "Book Livre", "Dictionary Dictionnaire", "Student Étudiante", "Teacher Professeur", "Math Mathématiques", "English Anglais", "History Histoire", "Science Science", "Homework Devoirs", "Pencilcase Trouse", "Eraser Gomme", "Protractor Rapporteur"]
-shuffle(schoolWords);
-let containersEnglish = ["school-english-word-1", "school-english-word-2", "school-english-word-3", "school-english-word-4", "school-english-word-5", "school-english-word-6"];
-let containersFrench = ["school-french-word-1", "school-french-word-2", "school-french-word-3", "school-french-word-4", "school-french-word-5", "school-french-word-6"];
-let shuffledContainersEnglish = shuffle(containersEnglish);
-let shuffledContainersFrench = shuffle(containersFrench);
-
-//to remove reandomly slected array item 
-let firstArray = schoolWords.shift();
-let secondArray = schoolWords.shift();
-let thirdArray = schoolWords.shift();
-let forthArray = schoolWords.shift();
-let fifthArray = schoolWords.shift();
-let sixthArray = schoolWords.shift();
-        
-/** code to split array items // 
-* https://stackoverflow.com/questions/40784961/get-all-string-in-one-variable-after-space-in-javascript
-*/        
-var values = firstArray.split(" ");
-var englishWordOne = values[0];
-var frenchWordOne = firstArray.substring(firstArray.indexOf(' ') + 1);
-        
-//to select random container 
-let englishContainerOne = containersEnglish.shift();
-let frenchContainerOne = containersFrench.shift();
-
-//to send random word to random container
-document.getElementById(englishContainerOne).textContent = englishWordOne;
-document.getElementById(frenchContainerOne).textContent = frenchWordOne;
-        
-//repeat of the above code 
-var values = secondArray.split(" ");
-var englishWordTwo = values[0];
-var frenchWordTwo = secondArray.substring(secondArray.indexOf(' ') + 1);
-        
-let englishContainerTwo = containersEnglish.shift();
-let frenchContainerTwo = containersFrench.shift();
-
-document.getElementById(englishContainerTwo).textContent = englishWordTwo;
-document.getElementById(frenchContainerTwo).textContent = frenchWordTwo;
-        
-var values = thirdArray.split(" ");
-var englishWordThree = values[0];
-var frenchWordThree = thirdArray.substring(thirdArray.indexOf(' ') + 1);
-        
-let englishContainerThree = containersEnglish.shift();
-let frenchContainerThree = containersFrench.shift();
-
-document.getElementById(englishContainerThree).textContent = englishWordThree;
-document.getElementById(frenchContainerThree).textContent = frenchWordThree;
-        
-var values = forthArray.split(" ");
-var englishWordFour = values[0];
-var frenchWordFour = forthArray.substring(forthArray.indexOf(' ') + 1);
-        
-let englishContainerFour = containersEnglish.shift();
-let frenchContainerFour = containersFrench.shift();
-
-document.getElementById(englishContainerFour).textContent = englishWordFour;
-document.getElementById(frenchContainerFour).textContent = frenchWordFour;
-        
-var values = fifthArray.split(" ");
-var englishWordFive = values[0];
-var frenchWordFive = fifthArray.substring(fifthArray.indexOf(' ') + 1);
-        
-let englishContainerFive = containersEnglish.shift();
-let frenchContainerFive = containersFrench.shift();
-
-document.getElementById(englishContainerFive).textContent = englishWordFive;
-document.getElementById(frenchContainerFive).textContent = frenchWordFive;
-        
-var values = sixthArray.split(" ");
-var englishWordSix = values[0];
-var frenchWordSix = sixthArray.substring(sixthArray.indexOf(' ') + 1);
-        
-let englishContainerSix = containersEnglish.shift();
-let frenchContainerSix = containersFrench.shift();
-
-document.getElementById(englishContainerSix).textContent = englishWordSix;
-document.getElementById(frenchContainerSix).textContent = frenchWordSix;
-// end of repeat 
-
-        
-//telling the programme that these words are equal 
-englishWordOne = frenchWordOne;
-englishWordTwo = frenchWordTwo;
-englishWordThree = frenchWordThree;
-englishWordFour = frenchWordFour;
-englishWordFive = frenchWordFive;
-englishWordSix = frenchWordSix;
-
-
 
 
 //the id of school-english-word-1 is randomised using shuffle. lets find the result and assess that// 
@@ -147,7 +118,6 @@ function activateAndAssess1 () {
 
         return wordOne;       
 }
-
 function activateAndAssess2 () {
         document.getElementById("school-french-word-1",).classList.toggle("selected-french");
         
@@ -157,8 +127,7 @@ function activateAndAssess2 () {
         var wordTwo = idAdd.innerHTML;
 
         return wordTwo;
-}
-        
+}        
 function activateAndAssess3 () {
         document.getElementById("school-english-word-2").classList.toggle("selected");
         
@@ -217,7 +186,6 @@ function activateAndAssess8 () {
 
         return wordTwo;
 }
-
 function activateAndAssess9 () {
         document.getElementById("school-english-word-5").classList.toggle("selected");
         
@@ -228,7 +196,6 @@ function activateAndAssess9 () {
 
         return wordOne;
 }
-
 function activateAndAssess10 () {
         document.getElementById("school-french-word-5",).classList.toggle("selected-french");
         
@@ -239,7 +206,6 @@ function activateAndAssess10 () {
 
         return wordTwo;
 }
-
 function activateAndAssess11 () {
         document.getElementById("school-english-word-6").classList.toggle("selected");
        
@@ -480,6 +446,17 @@ function FrenchEnglishTest () {
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
 //https://www.geeksforgeeks.org/extract-a-number-from-a-string-using-javascript/
 
 function numberFinder(a) {
@@ -504,7 +481,22 @@ function addingNumbersToString (a, b) {
         return result;
 }
 
-
+/** The Fisher-Yates shuffle
+ * https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+ * https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+ */
+function shuffle(array) {
+        let currentIndex = array.length;
+      
+        while (currentIndex != 0) {
+      
+          let randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+}
 
 
 
